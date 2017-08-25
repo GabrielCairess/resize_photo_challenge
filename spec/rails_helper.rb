86235217c77359b9042d2cbdac86eef96a50010d
Gabriel Caires <gbrlcaires@gmail.com>
2017-08-25 19:51:45 -0300

    tests corrections

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,17 +1,22 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-abort('The Rails environment is running in production mode!') if Rails.env.production?
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
-ActiveRecord::Migration.maintain_test_schema!
+require 'database_cleaner'
+require 'support/vcr_setup'
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.include FactoryGirl::Syntax::Methods
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
+  DatabaseCleaner.strategy = :truncation
+
+  config.after :each do
+    DatabaseCleaner.clean
+  end
+
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"])
+  end
 end
